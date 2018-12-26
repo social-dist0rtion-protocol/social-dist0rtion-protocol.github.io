fb7f0c28407b39f37e7eb7fcdbb18431e4cfc61c
Nicola Miotto <sirnicolaz@gmail.com>
2018-12-26 20:48:36 +0100

    auto update

---
+++ precache-manifest.f8145f2df2c69a5ea7efef9d8bf3a06f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f8db550becf605a4ae7",
+    "url": "./static/css/main.ccede82e.chunk.css"
+  },
+  {
+    "revision": "4f8db550becf605a4ae7",
+    "url": "./static/js/main.4f8db550.chunk.js"
+  },
+  {
+    "revision": "df7bdd8e7a3682ef1a76",
+    "url": "./static/js/1.df7bdd8e.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f86bde8f747dd05e021070cc3e606426",
+    "url": "./index.html"
+  }
+];
