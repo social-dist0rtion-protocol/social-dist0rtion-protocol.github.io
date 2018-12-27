050d3f74130b6ca4ae6e39a8e3f0987b659e2255
Nicola Miotto <sirnicolaz@gmail.com>
2018-12-27 20:01:30 +0100

    auto update

---
+++ precache-manifest.a3ba061444aba15416c4bd79245806e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0728a05724bf1c545bd2",
+    "url": "./static/css/main.857daa9b.chunk.css"
+  },
+  {
+    "revision": "0728a05724bf1c545bd2",
+    "url": "./static/js/main.0728a057.chunk.js"
+  },
+  {
+    "revision": "01527760aa039bc27781",
+    "url": "./static/js/1.01527760.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c6bd4720ad8f974a289ef48433f69e02",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.245261927edb1e6427a29b51656d4cd7.js"
+  "./precache-manifest.a3ba061444aba15416c4bd79245806e9.js"
 );
 
 workbox.clientsClaim();
